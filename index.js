1a826de4230ff58af28f4e1be82e70e944c2add7
Sean Graham <seangraham006@gmail.com>
2024-10-03 09:17:39 +0000

    added middleware to parse JSON and URL

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +19,10 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post("/api/shorturl", async (req,res) => {
+  console.log(req.body);
 });
 
 app.listen(port, function() {
